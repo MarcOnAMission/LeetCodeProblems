b8bf7d1b0478880b0d17e0ac51ae5818eb4b527f
Marc <marcmihai2005@gmail.com>
2025-11-20 16:54:39 +0200

    Problem number 14

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,12 @@
+import LongestPrefix.Solution;
+
+import java.sql.Array;
+import java.util.List;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
+        Solution.longestCommonPrefix();
     }
 }
